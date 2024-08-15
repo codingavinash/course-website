c0d993c4cc1bf80fd67af02a047bd0d8c2dfc25f
codingavinash <vishwakarmaavinash299@gmail.com>
2024-08-15 19:46:28 +0530

    Updated HeroSection with ' to &apos; because of error

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
+'use client';
 import Link from "next/link";
 import React from "react";
-import { cn } from "@/lib/utils";
 import { Spotlight } from "@/components/ui/Spotlight";
 import { Button } from "@/components/ui/moving-border";
 
@@ -15,7 +15,7 @@ const HeroSection = () => {
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of music</h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Dive into our comprehensive music courses and transform your musical
-          journey today. Whether you're a beginner or looking to refine your
+          journey today. Whether you&apos; a beginner or looking to refine your
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
